3d2257fd8bc0dbbf970022f28563b3d07744b014
Emiliano <emiquiroga@andreani.com>
2025-01-21 14:59:12 -0300

    feat: sample polygone

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,5 +1,5 @@
 import { Box, Typography } from "@mui/material";
-import { MapContainer, Marker, Popup, TileLayer } from "react-leaflet";
+import { MapContainer, Marker, Popup, TileLayer, Polygon } from "react-leaflet";
 import { LatLngExpression } from "leaflet";
 import { useEffect, useRef } from "react";
 
@@ -7,6 +7,14 @@ import "leaflet/dist/leaflet.css";
 import styles from "./styles.module.scss";
 
 export const BS_AS_POSITION: LatLngExpression = [-34.603722, -58.381592];
+
+const polygonCoordinates: LatLngExpression[] = [
+  [-34.615852, -58.435348], // Palermo
+  [-34.609481, -58.383188], // Recoleta
+  [-34.611, -58.38167], // Plaza de Mayo
+  [-34.643101, -58.449082], // La Boca
+  [-34.646927, -58.499861], // Parque Avellaneda
+];
 
 export default function Home() {
   const map = useRef<L.Map>(null);
@@ -32,13 +40,14 @@ export default function Home() {
           className={styles.mapContainer}
           fadeAnimation={true}
           scrollWheelZoom={true}
-          zoom={6}
+          zoom={10}
           zoomAnimation={true}
         >
           <TileLayer
             attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
             url={tileUrl}
           />
+          <Polygon pathOptions={{ color: "blue" }} positions={polygonCoordinates} />
           {/* <Marker position={[51.505, -0.09]}>
         <Popup>
           A pretty CSS3 popup. <br /> Easily customizable.
