ba0d07a99fd004cba0b905f8ba0bd8cb9372cfd4
Emiliano <emiquiroga@andreani.com>
2025-01-21 11:30:21 -0300

    base map

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,20 +1,48 @@
 import { Box, Typography } from "@mui/material";
 import { MapContainer, Marker, Popup, TileLayer } from "react-leaflet";
+import { LatLngExpression } from "leaflet";
+import { useEffect, useRef } from "react";
 
 import styles from "./styles.module.scss";
 
+export const BS_AS_POSITION: LatLngExpression = [-34.603722, -58.381592];
+
 export default function Home() {
+  const map = useRef<L.Map>(null);
+  const tileUrl = "https://tile.openstreetmap.org/{z}/{x}/{y}.png";
+
+  useEffect(() => {
+    if (!map?.current) return;
+
+    map.current.setMaxZoom(18);
+    map.current.setMinZoom(8);
+  }, [map]);
+
   return (
-    <MapContainer center={[51.505, -0.09]} scrollWheelZoom={false} zoom={13}>
-      <TileLayer
-        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-      />
-      <Marker position={[51.505, -0.09]}>
+    <div className={styles.root}>
+      <div>
+        <Typography variant="h1">Map App</Typography>
+        <Typography>Map App</Typography>
+      </div>
+      <MapContainer
+        ref={map}
+        center={BS_AS_POSITION}
+        className={styles.mapContainer}
+        fadeAnimation={true}
+        scrollWheelZoom={true}
+        zoom={6}
+        zoomAnimation={true}
+      >
+        <TileLayer
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          url={tileUrl}
+        />
+        {/* <Marker position={[51.505, -0.09]}>
         <Popup>
           A pretty CSS3 popup. <br /> Easily customizable.
         </Popup>
-      </Marker>
-    </MapContainer>
+      </Marker> */}
+      </MapContainer>
+    </div>
   );
 }
