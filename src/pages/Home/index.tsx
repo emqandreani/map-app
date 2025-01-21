ba3216c5dd0279b01caaa374a2c1a00321faaf89
Emiliano <emiquiroga@andreani.com>
2025-01-21 13:45:05 -0300

    fix: styles and app name

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -3,6 +3,7 @@ import { MapContainer, Marker, Popup, TileLayer } from "react-leaflet";
 import { LatLngExpression } from "leaflet";
 import { useEffect, useRef } from "react";
 
+import "leaflet/dist/leaflet.css";
 import styles from "./styles.module.scss";
 
 export const BS_AS_POSITION: LatLngExpression = [-34.603722, -58.381592];
@@ -24,25 +25,27 @@ export default function Home() {
         <Typography variant="h1">Map App</Typography>
         <Typography>Map App</Typography>
       </div>
-      <MapContainer
-        ref={map}
-        center={BS_AS_POSITION}
-        className={styles.mapContainer}
-        fadeAnimation={true}
-        scrollWheelZoom={true}
-        zoom={6}
-        zoomAnimation={true}
-      >
-        <TileLayer
-          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-          url={tileUrl}
-        />
-        {/* <Marker position={[51.505, -0.09]}>
+      <div className={styles.mapWrapper} id="map">
+        <MapContainer
+          ref={map}
+          center={BS_AS_POSITION}
+          className={styles.mapContainer}
+          fadeAnimation={true}
+          scrollWheelZoom={true}
+          zoom={6}
+          zoomAnimation={true}
+        >
+          <TileLayer
+            attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+            url={tileUrl}
+          />
+          {/* <Marker position={[51.505, -0.09]}>
         <Popup>
           A pretty CSS3 popup. <br /> Easily customizable.
         </Popup>
       </Marker> */}
-      </MapContainer>
+        </MapContainer>
+      </div>
     </div>
   );
 }
